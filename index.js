ed939d93049a48df2238ff01b34b92552fe3274c
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-24 23:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ for (var i = 0; i < cart.length; i++) {
 }
 
 function placeOrder(cardNumber) {
-if (isNaN(cardNumber) = ) {
+if (isNaN(cardNumber) === true) {
   console.log("Sorry, we don't have a credit card on file for you.")
 }
 }
