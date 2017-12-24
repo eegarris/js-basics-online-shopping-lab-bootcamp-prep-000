2cfb6fcaf78e1959cbfefb31611073bdeea17b3e
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-24 23:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ if (isNaN(cardNumber) === true) {
   console.log("Sorry, we don't have a credit card on file for you.")
 }
 else {
-  console.log(`Your total cost is $` + `${sum}` + `)
+  console.log(`Your total cost is $` + `${sum}` + `, which will be charged to the card ` + `${cardNumber}` + `.`)
+  
 }
 }
